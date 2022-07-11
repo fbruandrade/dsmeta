de6a6f91cac347aa1d6a73ca4b9bc7fc2240dd23
Felipe Brunelli de Andrade <fbruandrade@gmail.com>
2022-07-11 10:28:13 -0300

    Project created
    Signed-off-by: Felipe Brunelli de Andrade <fbruandrade@gmail.com>

---
+++ backend/src/test/java/dev/fandrade/dsmeta/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.fandrade.dsmeta;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
