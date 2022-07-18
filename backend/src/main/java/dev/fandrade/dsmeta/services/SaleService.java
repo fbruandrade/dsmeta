9c6a282c1e88350a71acf147b80a259ea4fa1b75
Felipe Brunelli de Andrade <fbruandrade@gmail.com>
2022-07-18 09:51:21 -0300

    Paginated Results
    Signed-off-by: Felipe Brunelli de Andrade <fbruandrade@gmail.com>

--- backend/src/main/java/dev/fandrade/dsmeta/services/SaleService.java
+++ backend/src/main/java/dev/fandrade/dsmeta/services/SaleService.java
@@ -3,8 +3,11 @@ package dev.fandrade.dsmeta.services;
 import dev.fandrade.dsmeta.entities.Sale;
 import dev.fandrade.dsmeta.repository.SaleRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
+import java.time.LocalDate;
 import java.util.List;
 
 @Service
@@ -13,7 +16,13 @@ public class SaleService {
     @Autowired
     private SaleRepository saleRepository;
 
-    public List<Sale> findSales() {
-        return saleRepository.findAll();
+    public Page<Sale> findSales(String minDate, String maxDate, Pageable pageable) {
+        if (minDate.isEmpty() || maxDate.isEmpty()) {
+            return saleRepository.findAll(pageable);
+        } else {
+            LocalDate min = LocalDate.parse(minDate);
+            LocalDate max = LocalDate.parse(maxDate);
+            return saleRepository.findSales(min, max, pageable);
+        }
     }
 }
